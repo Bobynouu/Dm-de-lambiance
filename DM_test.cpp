4f28445456ee3c78ec4871f38f43a0cdaa8e02fb
bsacco <45011933+bsacco@users.noreply.github.com>
2018-12-12 13:07:19 +0100

    Add files via upload

--- DM_test.cpp
+++ DM_test.cpp
@@ -13,30 +13,30 @@ MethIterative::MethIterative()
 MethIterative::~MethIterative()
 {}
 
-
+/////////////////////////////////////////////////////////
 ///////////////////// CLASSE MERE ///////////////////////
+/////////////////////////////////////////////////////////
 void MethIterative::MatrixInitialize(SparseMatrix<double> A)
 {
   _A.resize(A.rows(), A.cols());
-  _A = A;
+  _A = A;   // A prend la valeur de la matrice du système
 }
-
+/////////////////////////////////////////////////////////
 const VectorXd & MethIterative::GetIterateSolution() const
 {
-  return _x;
+  return _x;  // On récupère la solution itérée
 }
-
+/////////////////////////////////////////////////////////
 const VectorXd & MethIterative::GetResidu() const
 {
-  return _r;
+  return _r;  // On récupère le résidu
 }
-
+/////////////////////////////////////////////////////////
 const VectorXd & MethIterative::Getp() const
 {
-  return _p;
+  return _p;  // On récupère le vecteur p
 }
-
-// Écrit un fichier avec la solution au format Paraview
+/////////////////////////////////////////////////////////
 void MethIterative::saveSolution(int N ,string name_file , int n_iter , double residu)
 {
   ofstream mon_flux; // Contruit un objet "ofstream"
@@ -45,72 +45,114 @@ void MethIterative::saveSolution(int N ,string name_file , int n_iter , double r
 
   if(mon_flux)
   {
-      mon_flux<<n_iter<<residu<<endl;
+    mon_flux<<n_iter<<residu<<endl;
   }
   mon_flux.close();
 }
+/////////////////////////////////////////////////////////
+SparseMatrix<double>  MethIterative::create_mat(const string name_file_read, bool sym)
+  {
+    int N(0.);
+    SparseMatrix<double> A ;
+    ifstream mon_flux(name_file_read);
+    string ligne, colonne, valeur;
+    getline(mon_flux,ligne); //lit la première ligne qui ne nous intéresse pas
 
+    mon_flux >> N; //lit le premier mot de la ligne 2 correspond au nombre de lignes
+    int nonzero_elem;
+    mon_flux >> colonne; //lit le nombre de colonnes (valeur stockée inutilement)
+    mon_flux >> nonzero_elem; //lit le nombre d'élements non nuls
 
+    // Définition de la la matrice A en sparse.
+    A.resize(N,N);
+    vector<Triplet<double>> liste_elem;
+    for (int i = 0; i < nonzero_elem; i++)
+    {
+      mon_flux >> ligne;
+      mon_flux >> colonne;
+      mon_flux >> valeur;
+
+      int li = atoi(ligne.c_str());
+      int col = atoi(colonne.c_str());
+      double val = atof(valeur.c_str());
+
+      liste_elem.push_back({li-1,col-1,val});  //atoi pour passer de string à int et atof idem avec double
+      if ((colonne != ligne) && sym) // dans le cas d'une matrice symétrique seulement la moitié des éléments sont dans le fichier texte
+      {
+        liste_elem.push_back({col-1,li-1,val});
+      }
+    }
+    A.setFromTriplets(liste_elem.begin(),liste_elem.end());
+    mon_flux.close();
+
+    return A;
+  }
+/////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
 ///////////////////// Gradient Conjugué ///////////////////////
-// initialisation des données
+///////////////////////////////////////////////////////////////
 void GradientConj::Initialize(VectorXd x0, VectorXd b)
 {
+  // Initialisation
   _x = x0;
   _b = b;
   _r = _b - _A*_x;
-  _p = _r;                   // utile pour le GradientConj
+  _p = _r;
 
   ofstream mon_flux; // Contruit un objet "ofstream"
-  string name_file = ("/sol_"+to_string(_x.size())+"_grad_conj.txt");  //commande pour modifier le nom de chaque fichier
+  string name_file = ("/sol_"+to_string(_x.size())+"_Grad_Conj.txt");  //commande pour modifier le nom de chaque fichier
   mon_flux.open(name_file,ios::out);
 }
-
+///////////////////////////////////////////////////////////////
 void GradientConj::Advance(VectorXd z)
 {
   double alpha , gamma, stock_r;
-
-  stock_r = _r.dot(_r);
-
+  // Corps d'Advance
+  stock_r = _r.dot(_r);    // On stock la norme de rk (nécessaire au calcul de gamma)
   alpha   =  _r.dot(_r)/(z.dot(_p));
   _x +=  alpha*_p ;
   _r += - alpha*z ;
   gamma = _r.dot(_r)/stock_r;
   _p = _r + gamma*_p;
 }
+///////////////////////////////////////////////////////////////
 
-
-
+////////////////////////////////////////////////////////////
 ///////////////////// Residu Minimum ///////////////////////
+////////////////////////////////////////////////////////////
 void ResiduMin::Initialize(VectorXd x0, VectorXd b)
 {
+  // Initialisation
   _x = x0;
   _b = b;
   _r = _b - _A*_x;
 
   ofstream mon_flux; // Contruit un objet "ofstream"
-  string name_file = ("/sol_"+to_string(_x.size())+"_res_min.txt");  //commande pour modifier le nom de chaque fichier
+  string name_file = ("/sol_"+to_string(_x.size())+"_Res_Min.txt");  //commande pour modifier le nom de chaque fichier
   mon_flux.open(name_file,ios::out);
 }
-
-
+///////////////////////////////////////////////////////////////
 void ResiduMin::Advance(VectorXd z)
 {
   double alpha(0.);
+  // Corps d'Advance
   alpha = _r.dot(z)/z.dot(z);
-
   _x += alpha*_r;
   _r += - alpha*z;
 }
+///////////////////////////////////////////////////////////////
 
-
-
+/////////////////////////////////////////////////////////////////////
 ///////////////////// Gauss Seidel Symétrique ///////////////////////
+/////////////////////////////////////////////////////////////////////
 void SGS::Initialize(VectorXd x0, VectorXd b)
 {
+  // Initialisation
   _x = x0;
   _b = b;
   _r = _b - _A*_x;
-
+  // Formation de L et U
   SparseMatrix<double> U, L, D;
   U.resize(_A.rows(), _A.cols()), L.resize(_A.rows(),_A.cols()), D.resize(_A.rows(),_A.cols());
   _U.resize(_A.rows(), _A.cols()), _L.resize(_A.rows(), _A.cols());
@@ -130,29 +172,33 @@ void SGS::Initialize(VectorXd x0, VectorXd b)
   }
   _L = L*D;
   _U = U;
+  // On calcule y qui sera le même tout le long de la boucle "while"
+  VectorXd y_bis(_x.size());       // On va Résoudre ici LUy = b
+  y_bis = GetSolTriangInf(_L, b);  // Résolution L*y_bis = b
+  _y = GetSolTriangSup(_U, y_bis); // Résolution U*y = b
 
-  VectorXd y_bis(_x.size());
-  y_bis = GetSolTriangInf(_L, b);
-  _y = GetSolTriangSup(_U, y_bis);
-
+  ofstream mon_flux; // Contruit un objet "ofstream"
+  string name_file = ("/sol_"+to_string(_x.size())+"_SGS.txt");  //commande pour modifier le nom de chaque fichier
+  mon_flux.open(name_file,ios::out);
 }
-
+/////////////////////////////////////////////////////////////////////
 void SGS::Advance(VectorXd z)
 {
   VectorXd  w(_x.size()), w_bis(_x.size());
-
-  w_bis = GetSolTriangInf(_L, z);
-  w = GetSolTriangSup(_U, w_bis);
-
-  _x += - w + _y;
-  _r = _b - _A*_x;
-
+  // Corps d'Advance
+  w_bis = GetSolTriangInf(_L, z);  // Résolution de L*w_bis = z (avec z = A*xk)
+  w = GetSolTriangSup(_U, w_bis);  // Résolution de U*w = w_bis
+  _r = _b - z;    // On calcule d'abord le résidu avec xk et non xk+1 de manière à enlever un produit matrice/vecteur
+  _x += - w + _y;  // et donc de gagner considérablement en temps de calcul quitte à faire un tour de boucle supplémentaire
 }
+/////////////////////////////////////////////////////////////////////
 
-
+///////////////////////////////////////////////////
 ///////////////////// GMRes ///////////////////////
+///////////////////////////////////////////////////
 void Gmres::Initialize(VectorXd x0, VectorXd b)
 {
+  // Initialisation
   _x = x0;
   _b = b;
   _r = _b - _A*_x;
@@ -160,72 +206,65 @@ void Gmres::Initialize(VectorXd x0, VectorXd b)
   _Krylov = 10;
 
   ofstream mon_flux; // Contruit un objet "ofstream"
-  string name_file = ("/sol_"+to_string(_x.size())+"_grad_conj.txt");  //commande pour modifier le nom de chaque fichier
+  string name_file = ("/sol_"+to_string(_x.size())+"_GMRes.txt");  //commande pour modifier le nom de chaque fichier
   mon_flux.open(name_file,ios::out);
 }
-
-
+///////////////////////////////////////////////////
 void Gmres::Arnoldi( SparseMatrix<double> A , VectorXd v)
 {
   //dimension de l'espace
   int m = _Krylov;
-
-  _Vm.resize(v.size(), m+1);
-
-  vector< SparseVector<double> > Vm ;
+  // Initialisation
+  _Vm.resize(v.size(), m+1);    // Matrice Vm de l'espace de Krylov
+  vector< SparseVector<double> > Vm ;  // Vecteur utile au stock des valeurs
   SparseVector<double> v1 , s1 ;
   s1.resize(v.size());
-  v1 = v.sparseView();
-
-  _Hm.resize( m+1 , m );
+  _Hm.resize( m+1 , m );   // Matrice de Hessenberg
   _Hm.setZero();
-
   vector< SparseVector<double> > z;
   z.resize(_Krylov);
   Vm.resize(m+1);
-  Vm[0]= v1/v1.norm();
-  cout << (A*Vm[0]).size() << endl;
+
+  v1 = v.sparseView();
+  Vm[0]= v1/v1.norm(); // On affecte la premier vecteur de la base de Krylov à Vm+1
+  // Corps de l'algo d'Arnoldi
   for (int j=0 ; j<m ; j++)
     {SparseVector<double> Av = A*Vm[j];
       s1.setZero();
-
         for(int i=0 ; i<j+1 ; i++)
         {
-
           _Hm.coeffRef(i,j) = Av.dot(Vm[i]);
           s1 = s1 + _Hm.coeffRef(i,j)*Vm[i];
         }
       z[j] = Av - s1;
       _Hm.coeffRef(j+1,j) = z[j].norm();
+    // Si il y a un zéro sur la sous diagonale
     if(_Hm.coeffRef(j+1,j) == 0.)
     {   exit(0);}
 
-    Vm[j+1] = z[j]/_Hm.coeffRef(j+1,j);
+    Vm[j+1] = z[j]/_Hm.coeffRef(j+1,j); // Afection du J ième vecteur de la base de Krylov
   }
-
+  // Affectation des valeurs à la matrice Vm+1
   for(int i=0; i<v.size(); i++)
   {
     for (int j=0; j<m+1; j++)
     {_Vm.coeffRef(i,j) = Vm[j].coeffRef(i);}
   }
-
 }
-
-
+///////////////////////////////////////////////////
 void Gmres::Givens(SparseMatrix<double> Hm)
 {
-
-// J'ai mis R et Q de taille carré...
+  // Initialisation
   _Rm = Hm;
-  //cout << Hm << endl;
   _Qm.resize(Hm.rows(), Hm.rows());
-  //cout << "QM " << _Qm.rows() << _Qm.cols() << endl;
   _Qm.setIdentity();
   double c(0.), s(0.), u(0.), v(0.);
   SparseMatrix<double> Rotation_transposee(Hm.rows(), Hm.rows());
 
+  // Traitement de R
   for (int i=0; i<Hm.rows()-1; i++)
   {
+    // Formation des coefficients de rotation
     Rotation_transposee.setIdentity();
     c = _Rm.coeffRef(i,i)/sqrt(_Rm.coeffRef(i,i)*_Rm.coeffRef(i,i) + _Rm.coeffRef(i+1,i)*_Rm.coeffRef(i+1,i));
     s = -_Rm.coeffRef(i+1,i)/sqrt(_Rm.coeffRef(i,i)*_Rm.coeffRef(i,i) + _Rm.coeffRef(i+1,i)*_Rm.coeffRef(i+1,i));
@@ -233,88 +272,80 @@ void Gmres::Givens(SparseMatrix<double> Hm)
     Rotation_transposee.coeffRef(i+1,i+1) = c;
     Rotation_transposee.coeffRef(i+1,i) = -s;
     Rotation_transposee.coeffRef(i,i+1) = s;
-
+    // Mise à jour de R
     for (int j=i; j<Hm.cols(); j++)
     {
       u = _Rm.coeffRef(i,j);
       v = _Rm.coeffRef(i+1,j);
       _Rm.coeffRef(i,j) = c*u - s*v;
       _Rm.coeffRef(i+1,j) = s*u + c*v;
-      //if (j == i)
-      //{cout << "ici " << _Rm.coeffRef(i+1,j) << endl;}
     }
-
+    // Mise à jour de Q
     _Qm = _Qm*Rotation_transposee;
-    //cout << "Rm rows / cols" << _Rm.rows() << " " << _Rm.cols() << endl;
   }
-    // cout << "_vm = "<< _Vm << endl;
 }
-
-
+///////////////////////////////////////////////////
 const SparseMatrix<double> & Gmres::GetVm() const
 {
-  return _Vm;
+  return _Vm;  // On récupère Vm
 }
-
+///////////////////////////////////////////////////
 const SparseMatrix<double> & Gmres::GetHm() const
 {
-  return _Hm;
+  return _Hm;  // On récupère Hm
 }
-
+///////////////////////////////////////////////////
 void Gmres::Advance(VectorXd z)
 {
+  // Initialisation
   VectorXd gm_barre(_Qm.rows()), gm(_Krylov), y(_Krylov), vect(_Qm.rows());
   SparseMatrix<double> Rm_pas_barre(_Rm.cols(), _Rm.cols());
   SparseMatrix<double> Vm;
   Vm.resize(z.size(), _Krylov);
-
   gm.setZero();
   gm_barre.setZero();
   vect.setZero();
 
   for (int i=0; i<_Qm.rows(); i++)
   {
-    gm_barre[i] = _Qm.coeffRef(0,i);
-    vect[i] = _Qm.coeffRef(i,_Krylov);
+    gm_barre[i] = _Qm.coeffRef(0,i); // On forme le vecteur gm_barre
+    vect[i] = _Qm.coeffRef(i,_Krylov); // Vecteur utile pour la mise à jour de rk
   }
 
-  gm_barre = z.norm()*gm_barre;
+  gm_barre = z.norm()*gm_barre;  // gm_barre recoit sa valeur finale
 
   for (int i=0; i<_Krylov; i++)
   {
-    gm[i] = gm_barre[i];
+    gm[i] = gm_barre[i];   // On extrait gm à partir de gm_barre (on rejette le dernier terme de gm_barre)
 
     for (int j=0; j<_Krylov; j++)
     {
-      Rm_pas_barre.coeffRef(i,j) = _Rm.coeffRef(i,j);
+      Rm_pas_barre.coeffRef(i,j) = _Rm.coeffRef(i,j); // On extrait Rm (c'est Rm_barre sans la dernière ligne)
     }
     for (int j=0; j<Vm.rows(); j++)
     {
-      Vm.coeffRef(j,i) = _Vm.coeffRef(j,i);
+      Vm.coeffRef(j,i) = _Vm.coeffRef(j,i); // On extrait Vm à partir de Vm+1
     }
   }
 
-  y = GetSolTriangSup(Rm_pas_barre, gm);
+  y = GetSolTriangSup(Rm_pas_barre, gm); // Résolution de Rm*y = gm
 
-  _x = _x + Vm*y;
+  _x = _x + Vm*y; // Mise à jour de xk
 
-  _r = gm_barre[_Krylov]*_Vm*vect;
+  _r = gm_barre[_Krylov]*_Vm*vect;  // Mise à jour de rk
 
-  _beta = abs(gm_barre[_Krylov]);
-    //cout << " r = " << _r << endl;
-    cout << "gm+1 " << gm_barre[_Krylov] << endl;
-    cout <<"norme de r " << _r.norm() << endl;
-  // cout << "juste après l'affectation de r dans advance" << endl;
-  //
-  // cout << "_vm = "<< _Vm << endl;
+  _beta = abs(gm_barre[_Krylov]);  // On extrait la norme de rk
 }
+///////////////////////////////////////////////////
 const double & Gmres::GetNorm() const
 {
-  return _beta;
+  return _beta;  // Récupère la norme du résidu (rk)
 }
+///////////////////////////////////////////////////
 
-
+///////////////////////////////////////////////////////////////////
 ///////////////////// Fonctions hors classe ///////////////////////
+///////////////////////////////////////////////////////////////////
 VectorXd GetSolTriangSup(SparseMatrix<double> U, VectorXd b)
 {
   VectorXd solution(U.rows());
@@ -330,7 +361,7 @@ VectorXd GetSolTriangSup(SparseMatrix<double> U, VectorXd b)
   }
   return solution;
 }
-
+///////////////////////////////////////////////////////////////////
 VectorXd GetSolTriangInf(SparseMatrix<double> L, VectorXd b)
 {
   VectorXd solution(L.rows());
@@ -346,6 +377,7 @@ VectorXd GetSolTriangInf(SparseMatrix<double> L, VectorXd b)
   }
   return solution;
 }
+/////////////////////////////////////////////////////////////////////
 
 #define _Meth_Iterative_H
 #endif
--- DM_test.h
+++ DM_test.h
@@ -18,26 +18,29 @@ class MethIterative
     Eigen::VectorXd _x;  // solution itérée
     Eigen::VectorXd _r;  // vecteur résidu
     Eigen::VectorXd _b;  // vecteur du second membre
-    Eigen::VectorXd _p;
     Eigen::SparseMatrix<double> _A; // matrice du systeme
+    Eigen::VectorXd _p;   // nécessaire à l'algorithme du Gradient Conjugue
 
   public:
     // constructeur à partir d'une matrice A dense donnée
     MethIterative();
     // destructeur
     virtual ~MethIterative();
+    // crée une matrice à partir d'un fichier de MatrixMarket
+    Eigen::SparseMatrix<double> create_mat(const std::string name_file_read, bool sym);
     // initialise une Matrice
     void MatrixInitialize(Eigen::SparseMatrix<double> A);
     // initialise les données
     virtual void Initialize(Eigen::VectorXd x0, Eigen::VectorXd b)=0;
-    // initialise la matrice A venant d'un fichier
-    //SparseMatrix<double> MatrixInitialize(const std::string name_file_read, bool sym);
     // exécute une itération
     virtual void Advance(Eigen::VectorXd z) = 0;
-    // récupère le vecteur xk et le vecteur rk
+    // récupère le vecteur xk
     const Eigen::VectorXd & GetIterateSolution() const;
+    // Récupère le Résidu
     const Eigen::VectorXd & GetResidu() const;
-    const Eigen::VectorXd & Getp() const;
+    // Récupère le vecteur p
+    virtual const Eigen::VectorXd & Getp() const;
+    // Sauvgarde la norme du résidu dans un fichier
     void saveSolution(int N , std::string name_file ,  int n_iter , double residu);
 
 
@@ -46,52 +49,64 @@ class MethIterative
 class ResiduMin : public MethIterative
 {
   public:
+    // exécute une itération
     void Advance(Eigen::VectorXd z);
+    // initialise les données
     void Initialize(Eigen::VectorXd x0, Eigen::VectorXd b);
 };
 
 class GradientConj : public MethIterative
 {
   public:
+    // exécute une itération
     void Advance(Eigen::VectorXd z);
+    // initialise les données
     void Initialize(Eigen::VectorXd x0, Eigen::VectorXd b);
 };
 
 class SGS : public MethIterative
 {
   private:
-    Eigen::SparseMatrix<double> _L;
-    Eigen::SparseMatrix<double> _U;
-    Eigen::SparseMatrix<double> _M;
-    Eigen::VectorXd _y;
+    Eigen::SparseMatrix<double> _L;   // Matrice égale à la partie triangulaire inférieure de M
+    Eigen::SparseMatrix<double> _U;   // Matrice égale à la partie triangulaire supérieure de M
+    Eigen::VectorXd _y;               // Stock un vecteur dans SGS
   public:
+    // initialise les données
     void Initialize(Eigen::VectorXd x0, Eigen::VectorXd b);
+    // exécute une itération
     void Advance(Eigen::VectorXd z);
 };
 
 class Gmres : public MethIterative
 {
   private:
-    Eigen::SparseMatrix<double> _Vm ;
-    Eigen::SparseMatrix<double> _Hm;
-    Eigen::SparseMatrix<double> _Qm;
-    Eigen::SparseMatrix<double> _Rm;
-    double _beta;
-    int _Krylov;
+    Eigen::SparseMatrix<double> _Vm ;  // Matrice de l'espace de Krylov via Arnoldi
+    Eigen::SparseMatrix<double> _Hm;   // Matrice d'Hessenberg via Arnoldi
+    Eigen::SparseMatrix<double> _Qm;   // Matrice Q de la décomposition QR via Givens
+    Eigen::SparseMatrix<double> _Rm;   // Matrice R de la décomposition QR via Givens
+    double _beta;   // Stock la norme du résidu après une itération
+    int _Krylov;    // Dimension de l'espace de Krylov
   public:
+    // Récupère la matrice d'Hessenberg
     const Eigen::SparseMatrix<double> & GetHm() const;
+    // Récupère la matrice de l'espace de Krylov
     const Eigen::SparseMatrix<double> & GetVm() const;
+    // Récupère la norme du résidu
     const double & GetNorm() const;
+    // exécute une itération
     void Advance(Eigen::VectorXd z);
+    // initialise les données
     void Initialize(Eigen::VectorXd x0, Eigen::VectorXd b);
+    // Exécute l'algorithme d'Arnoldi à partir de A et le résidu, on obtient une matrice d'Hessenberg
     void Arnoldi(Eigen::SparseMatrix<double> A, Eigen::VectorXd v);
+    // Exécute la décomposition QR d'une matrice d'Hessenberg via Givens
     void Givens(Eigen::SparseMatrix<double> Hm);
 };
 
+// Résolution d'un système linéaire pour une matrice triangulaire supérieure
 Eigen::VectorXd GetSolTriangSup(Eigen::SparseMatrix<double> U, Eigen::VectorXd b);
+// Résolution d'un système linéaire pour une matrice triangulaire inférieure
 Eigen::VectorXd GetSolTriangInf(Eigen::SparseMatrix<double> L, Eigen::VectorXd b);
-
-
 
 #define _Meth_Iterative_H
 #endif
